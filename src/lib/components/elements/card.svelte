e1abfadce93ff43df879124d71071a16cd5bf549
Kawsar Ahmed <prokawsar@gmail.com>
2023-10-23 19:21:05 +0600

    icon change

--- src/lib/components/elements/card.svelte
+++ src/lib/components/elements/card.svelte
@@ -5,8 +5,8 @@
 <div
 	class="shadow-md rounded-xl bg-white flex flex-col gap-3 items-center w-full justify-center h-56"
 >
-	<div class="bg-gray-200 rounded-full h-24 w-24">
-		<Security />
-	</div>
+	<!-- <div class="bg-gray-200 rounded-full h-24 w-24"> -->
+	<Security />
+	<!-- </div> -->
 	<p>something</p>
 </div>
--- src/lib/icons/security.svelte
+++ src/lib/icons/security.svelte
@@ -1,23 +1,24 @@
-<svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<g filter="url(#filter0_iiii_1_1723)">
+<svg width="110" height="110" viewBox="0 0 110 110" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<circle cx="55" cy="55" r="55" fill="#F1F1F1" />
+	<g filter="url(#filter0_iiii_1_1721)">
 		<path
-			d="M47.5093 95.1789C48.7294 95.8366 50.2644 95.839 51.4901 95.1918C84.8926 77.449 87.407 42.5156 87.4198 32.5797C87.42 31.7812 87.1871 30.9998 86.7496 30.3315C86.3121 29.6631 85.689 29.1367 84.9565 28.8167L51.534 13.8552C51.0021 13.6183 50.4265 13.495 49.8441 13.4932C49.2618 13.4914 48.6855 13.6112 48.1522 13.8449L14.8374 28.5213C14.1131 28.8352 13.4944 29.3505 13.055 30.0058C12.6157 30.6612 12.3741 31.4288 12.3592 32.2175C12.1761 42.1027 14.0207 77.1295 47.5093 95.1789Z"
-			fill="url(#paint0_linear_1_1723)"
+			d="M52.5093 100.179C53.7294 100.837 55.2644 100.839 56.4901 100.192C89.8926 82.449 92.407 47.5156 92.4198 37.5797C92.42 36.7812 92.1871 35.9998 91.7496 35.3315C91.3121 34.6631 90.689 34.1367 89.9565 33.8167L56.534 18.8552C56.0021 18.6183 55.4265 18.495 54.8441 18.4932C54.2618 18.4914 53.6855 18.6112 53.1522 18.8449L19.8374 33.5213C19.1131 33.8352 18.4944 34.3505 18.055 35.0058C17.6157 35.6612 17.3741 36.4288 17.3592 37.2175C17.1761 47.1027 19.0207 82.1295 52.5093 100.179Z"
+			fill="url(#paint0_linear_1_1721)"
 		/>
 	</g>
-	<g filter="url(#filter1_dii_1_1723)">
+	<g filter="url(#filter1_dii_1_1721)">
 		<path
 			fill-rule="evenodd"
 			clip-rule="evenodd"
-			d="M66.2824 37.8263C68.0028 39.5352 68.0107 42.3138 66.3001 44.0325L47.9355 62.4839C46.2739 64.1534 43.5897 64.2156 41.8524 62.6249L33.3261 54.8184C31.5375 53.1808 31.4164 50.4048 33.0557 48.618C34.6949 46.8312 37.4737 46.7103 39.2623 48.3479L44.6792 53.3076L60.0699 37.844C61.7805 36.1253 64.5619 36.1174 66.2824 37.8263Z"
+			d="M71.2824 42.8263C73.0028 44.5352 73.0107 47.3138 71.3001 49.0325L52.9355 67.4839C51.2739 69.1534 48.5897 69.2156 46.8524 67.6249L38.3261 59.8184C36.5375 58.1808 36.4164 55.4048 38.0557 53.618C39.6949 51.8312 42.4737 51.7103 44.2623 53.3479L49.6792 58.3076L65.0699 42.844C66.7805 41.1253 69.5619 41.1174 71.2824 42.8263Z"
 			fill="white"
 		/>
 	</g>
 	<defs>
 		<filter
-			id="filter0_iiii_1_1723"
-			x="2.34875"
-			y="3.49316"
+			id="filter0_iiii_1_1721"
+			x="7.34875"
+			y="8.49316"
 			width="95.071"
 			height="102.182"
 			filterUnits="userSpaceOnUse"
@@ -35,7 +36,7 @@
 			<feGaussianBlur stdDeviation="25" />
 			<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
 			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.575 0 0 0 0 0.527083 0 0 0 1 0" />
-			<feBlend mode="normal" in2="shape" result="effect1_innerShadow_1_1723" />
+			<feBlend mode="normal" in2="shape" result="effect1_innerShadow_1_1721" />
 			<feColorMatrix
 				in="SourceAlpha"
 				type="matrix"
@@ -46,7 +47,7 @@
 			<feGaussianBlur stdDeviation="10" />
 			<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
 			<feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.3 0" />
-			<feBlend mode="normal" in2="effect1_innerShadow_1_1723" result="effect2_innerShadow_1_1723" />
+			<feBlend mode="normal" in2="effect1_innerShadow_1_1721" result="effect2_innerShadow_1_1721" />
 			<feColorMatrix
 				in="SourceAlpha"
 				type="matrix"
@@ -57,7 +58,7 @@
 			<feGaussianBlur stdDeviation="10" />
 			<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
 			<feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.3 0" />
-			<feBlend mode="normal" in2="effect2_innerShadow_1_1723" result="effect3_innerShadow_1_1723" />
+			<feBlend mode="normal" in2="effect2_innerShadow_1_1721" result="effect3_innerShadow_1_1721" />
 			<feColorMatrix
 				in="SourceAlpha"
 				type="matrix"
@@ -71,12 +72,12 @@
 				type="matrix"
 				values="0 0 0 0 0.231476 0 0 0 0 0.829167 0 0 0 0 0.829167 0 0 0 0.3 0"
 			/>
-			<feBlend mode="normal" in2="effect3_innerShadow_1_1723" result="effect4_innerShadow_1_1723" />
+			<feBlend mode="normal" in2="effect3_innerShadow_1_1721" result="effect4_innerShadow_1_1721" />
 		</filter>
 		<filter
-			id="filter1_dii_1_1723"
-			x="17.1084"
-			y="28.3315"
+			id="filter1_dii_1_1721"
+			x="22.1084"
+			y="33.3315"
 			width="65.2624"
 			height="56.8141"
 			filterUnits="userSpaceOnUse"
@@ -92,8 +93,8 @@
 			<feOffset dy="6.57461" />
 			<feGaussianBlur stdDeviation="7.39643" />
 			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0" />
-			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_1723" />
-			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_1723" result="shape" />
+			<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_1721" />
+			<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_1721" result="shape" />
 			<feColorMatrix
 				in="SourceAlpha"
 				type="matrix"
@@ -104,7 +105,7 @@
 			<feGaussianBlur stdDeviation="4.10913" />
 			<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
 			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
-			<feBlend mode="normal" in2="shape" result="effect2_innerShadow_1_1723" />
+			<feBlend mode="normal" in2="shape" result="effect2_innerShadow_1_1721" />
 			<feColorMatrix
 				in="SourceAlpha"
 				type="matrix"
@@ -115,14 +116,14 @@
 			<feGaussianBlur stdDeviation="4.93096" />
 			<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
 			<feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.85 0" />
-			<feBlend mode="normal" in2="effect2_innerShadow_1_1723" result="effect3_innerShadow_1_1723" />
+			<feBlend mode="normal" in2="effect2_innerShadow_1_1721" result="effect3_innerShadow_1_1721" />
 		</filter>
 		<linearGradient
-			id="paint0_linear_1_1723"
-			x1="82.3689"
-			y1="91.0485"
-			x2="11.2438"
-			y2="21.6009"
+			id="paint0_linear_1_1721"
+			x1="87.3689"
+			y1="96.0485"
+			x2="16.2438"
+			y2="26.6009"
 			gradientUnits="userSpaceOnUse"
 		>
 			<stop stop-color="#66CCC6" />
