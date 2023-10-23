b504bb7271b401a905a4912d504e2c6fd5d08310
Kawsar Ahmed <prokawsar@gmail.com>
2023-10-23 20:57:48 +0600

    fixed hero

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -1,18 +1,19 @@
-<div
-	class="relative flex justify-center bg-[url('/img/hero.png')] bg-contain bg-no-repeat w-full h-full"
->
+<div class="relative w-full h-full">
 	<span class="absolute w-full h-full backdrop-brightness-75" />
-	<!-- <img src="img/hero.png" alt="hero" /> -->
-	<div class="flex flex-row h-[600px] justify-start z-10 w-full max-w-7xl">
-		<div class="flex flex-1 flex-col gap-5 justify-center items-center">
-			<h1 class="text-white text-5xl font-bold">
-				펫세이프는, 반려동물의 안전과 보호를 위한 인증제도 입니다
-			</h1>
-			<p class="text-xl font-normal text-white">
-				국제공인분석기관의 엄격한 테스트를 통과한, 안전하고 신뢰할 수 있는 제품과 서비스에만
-				수여하는 반려동물 생활용품 안전인증 마크입니다.
-			</p>
+	<img src="img/hero.png" alt="hero" class="h-auto max-w-full" />
+
+	<div class="absolute inset-y-0 left-0 flex items-center w-full justify-center">
+		<div class="flex flex-row w-full max-w-7xl">
+			<div class="flex-1">
+				<h1 class="text-white text-5xl font-bold">
+					펫세이프는, 반려동물의 안전과 보호를 위한 인증제도 입니다
+				</h1>
+				<p class="text-xl font-normal text-white">
+					국제공인분석기관의 엄격한 테스트를 통과한, 안전하고 신뢰할 수 있는 제품과 서비스에만
+					수여하는 반려동물 생활용품 안전인증 마크입니다.
+				</p>
+			</div>
+			<div class="flex-1" />
 		</div>
-		<div class="flex-1" />
 	</div>
 </div>
