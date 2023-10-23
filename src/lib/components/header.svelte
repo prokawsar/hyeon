4901c833c67a246ea09ec1d62773d4777b8f7fc1
Kawsar Ahmed <prokawsar@gmail.com>
2023-10-23 18:46:16 +0600

    fix

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -7,10 +7,10 @@
 </script>
 
 <div class="flex flex-row w-full py-4 px-3 md:px-0 max-w-4xl">
-	<div class="flex items-center mr-20">
-		<img src="img/logo.png" alt="logo" />
+	<div class="flex items-center mr-20 w-full h-full">
+		<img src="img/logo.png" alt="logo" class="" />
 	</div>
-	<div class="hidden md:flex flex-row gap-12 items-center justify-start">
+	<div class="hidden md:flex flex-row gap-12 items-center w-full justify-start">
 		{#each menus as menu}
 			<button
 				on:click={() => (activeMenu = menu)}
@@ -18,13 +18,13 @@
 				>{menu}</button
 			>
 		{/each}
-	</div>
-	<div class="hidden md:flex flex-row gap-2 w-full items-center justify-end">
-		<button> sign in </button>
-		<span>|</span>
-		<button> sign up </button>
+		<div class="flex flex-row w-60 gap-2 justify-end">
+			<button> sign in </button>
+			<span>|</span>
+			<button> sign up </button>
+		</div>
 	</div>
 	<div class="flex items-center justify-end w-full">
-		<Fa icon={faBars} size="1.2x" class="ml-14 cursor-pointer" />
+		<Fa icon={faBars} size="1.2x" class="cursor-pointer" />
 	</div>
 </div>
