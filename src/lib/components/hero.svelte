9439a51fecd1c592c13d5ee53d5469c2bb9e0814
Kawsar Ahmed <prokawsar@gmail.com>
2023-10-24 22:06:24 +0600

    hero text fix

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -4,12 +4,12 @@
 
 	<div class="absolute inset-y-0 left-0 flex items-center w-full justify-center">
 		<div class="flex flex-col md:flex-row w-full max-w-7xl px-5">
-			<div class="flex-1">
+			<div class="flex-1 flex flex-col gap-8">
 				<h1 class="text-white text-5xl font-bold">
-					펫세이프는, 반려동물의 안전과 보호를 위한 인증제도 입니다
+					펫세이프는,<br /> 반려동물의 안전과 보호를 위한 인증제도 입니다
 				</h1>
-				<p class="text-xl font-medium text-white">
-					국제공인분석기관의 엄격한 테스트를 통과한, 안전하고 신뢰할 수 있는 제품과 서비스에만
+				<p class="text-xl font-medium text-slate-200">
+					국제공인분석기관의 엄격한 테스트를 통과한, <br /> 안전하고 신뢰할 수 있는 제품과 서비스에만
 					수여하는 반려동물 생활용품 안전인증 마크입니다.
 				</p>
 			</div>
