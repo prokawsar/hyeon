46bb8de8083192656a65989d7227a8d89cdf242f
prokawsar <prokawsar@gmail.com>
2023-10-26 23:04:11 -0400

    text fix

--- src/lib/components/contact.svelte
+++ src/lib/components/contact.svelte
@@ -6,10 +6,10 @@
 	<div
 		class="flex flex-col md:flex-row p-4 gap-5 justify-between bg-white rounded-2xl shadow-xl w-full">
 		<div class="flex flex-col justify-center gap-3">
-			<p class="text-3xl font-semibold text-primary-500 mb-5">고객지원센터</p>
-			<span class="bg-gray-10 rounded-3xl px-4 py-1 text-lg font-normal"
+			<p class="text-xl md:text-3xl font-semibold text-primary-500 mb-5">고객지원센터</p>
+			<span class="bg-gray-10 rounded-3xl px-4 py-1 text-base md:text-lg font-normal"
 				>Tel. <tel>02-123-4567</tel></span>
-			<span class="bg-gray-10 rounded-3xl px-4 py-1 text-lg font-normal"
+			<span class="bg-gray-10 rounded-3xl px-4 py-1 text-base md:text-lg font-normal"
 				>E-mail. acdef@petsafe.co.kr</span>
 		</div>
 
