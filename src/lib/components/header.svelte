7130f035ee08efc6c34a81c8f54a19e5075d1ebf
prokawsar <prokawsar@gmail.com>
2023-10-26 22:07:03 -0400

    fixes header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -12,17 +12,16 @@
 </script>
 
 <div class="flex flex-row w-full py-4 px-5 justify-between md:px-3 max-w-7xl">
-	<div class="flex items-center w-[35%] h-full">
+	<a class="flex items-center w-[35%] h-full" href="/">
 		<img src="img/logo.png" alt="logo" class="" />
-	</div>
+	</a>
 	<div class="hidden md:flex flex-row gap-12 items-center w-full justify-start">
 		{#each menus as menu}
 			<button
 				on:click={() => (activeMenu = menu.name)}
 				class="text-xl font-medium {activeMenu == menu.name
 					? 'border-b-2 border-b-primary-400 text-primary-400'
-					: ''}">{menu.name}</button
-			>
+					: ''}">{menu.name}</button>
 		{/each}
 		<!-- <div class="flex flex-row w-60 gap-2 justify-end">
 			<button> sign in </button>
@@ -30,12 +29,12 @@
 			<button> sign up </button>
 		</div> -->
 	</div>
-	<div class="flex items-center justify-end md:justify-between w-[30%]">
+	<div class="flex items-center justify-end w-[30%]">
 		<div class="hidden md:flex flex-row gap-3 text-sm font-normal">
 			<button> 로그인 </button>
 			<span>|</span>
 			<button> 회원가입 </button>
 		</div>
-		<Fa icon={faBars} size="1.2x" class="cursor-pointer" />
+		<Fa icon={faBars} size="1.2x" class="cursor-pointer sm:hidden" />
 	</div>
 </div>
