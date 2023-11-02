8cadd2af67aa05b9f272dc4eed183ea23d0899e9
hwr12 <ruehyeon12@gmail.com>
2023-11-02 15:27:19 +0900

    text line break

--- src/lib/components/view/pages/home/hero.svelte
+++ src/lib/components/view/pages/home/hero.svelte
@@ -6,11 +6,14 @@
 		<div
 			class="flex flex-col md:flex-row w-full px-5 md:max-w-3xl lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl">
 			<div class="flex-1 flex flex-col gap-8">
-				<h1 class="text-white text-4xl sm:text-5xl font-bold">
-					펫세이프는,<br /> 반려동물의 안전과 보호를 위한 인증제도 입니다
+				<h1 class="text-white text-3xl sm:text-5xl font-bold whitespace-pre-line">
+					펫세이프는,
+					반려동물의 안전과 보호를
+					위한 인증제도 입니다
 				</h1>
-				<p class="text-xl font-medium text-slate-200">
-					국제공인분석기관의 엄격한 테스트를 통과한, <br /> 안전하고 신뢰할 수 있는 제품과 서비스에만
+				<p class="text-xl font-medium text-slate-200 whitespace-pre-line">
+					국제공인분석기관의 엄격한 테스트를 통과한,
+					안전하고 신뢰할 수 있는 제품과 서비스에만
 					수여하는 반려동물 생활용품 안전인증 마크입니다.
 				</p>
 			</div>
