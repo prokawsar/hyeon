bda43bcb545d316394519a2cb04b22e187147169
Kawsar Ahmed <prokawsar@gmail.com>
2023-11-02 18:23:29 +0600

    cs hero

---
+++ src/lib/components/view/pages/customerservice/CsHero.svelte
@@ -0,0 +1,10 @@
+<div class="flex bg-primary-hero w-full h-[250px] items-center px-[10%] justify-between">
+	<div class="flex flex-col sm:flex-row">
+		<div class="text-white font-bold text-xl whitespace-pre">
+			{'펫세이프 게시판 입니다 '}
+		</div>
+		<div class="text-white font-bold text-xl">펫세이프 게시판 입니다</div>
+	</div>
+
+	<img src="/svg/book.svg" alt="safetycerticon" class="h-[180px]" />
+</div>
--- src/routes/(pages)/customerservice/+page.svelte
+++ src/routes/(pages)/customerservice/+page.svelte
@@ -3,11 +3,13 @@
 	import { Section } from 'flowbite-svelte-blocks'
 	import MainTab from '$lib/components/view/pages/customerservice/MainTab.svelte'
 	import { csTabs } from '$lib/utils/const'
+	import CsHero from '$lib/components/view/pages/customerservice/CsHero.svelte'
 
 	let activeTab = csTabs[0]
 </script>
 
 <div class="flex flex-col justify-center items-center w-full mx-auto">
+	<CsHero />
 	<MainTab bind:activeTab />
 	{#if activeTab == csTabs[0]}
 		<div>Notice</div>
