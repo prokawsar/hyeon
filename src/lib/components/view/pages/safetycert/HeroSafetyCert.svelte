37e572a633c36227a8e0017a0ebbc7e33aacdd96
hwr12 <ruehyeon12@gmail.com>
2023-11-02 20:16:58 +0900

    인증품목을 확인해보세요 responsive

--- src/lib/components/view/pages/safetycert/HeroSafetyCert.svelte
+++ src/lib/components/view/pages/safetycert/HeroSafetyCert.svelte
@@ -1,5 +1,11 @@
 
 <div class="flex bg-primary-hero w-full h-[250px] items-center px-[10%] justify-between">
-    <div class="text-white font-bold text-xl">인증품목을 확인해보세요</div>
+    <div class="flex flex-col sm:flex-row">
+        <div class="text-white font-bold text-xl whitespace-pre">
+            {'인증품목을 '}</div>
+        <div class="text-white font-bold text-xl">
+            확인해보세요</div>
+    </div>
+
     <img src="/svg/safety_cert_hero_icon.svg" alt="safetycerticon" class="h-[180px]"/>
 </div>
