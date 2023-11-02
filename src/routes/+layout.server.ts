9f2801decce096719a45244c37b7c07745655477
hwr12 <ruehyeon12@gmail.com>
2023-11-02 15:52:14 +0900

    vercel test

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,3 +2,15 @@
 It is needed in order to circumvent a current limitation of SvelteKit. See https://github.com/inlang/inlang/issues/647
 You can remove this comment and modify the file as you like. We just need to make sure it exists.
 Please do not delete it (inlang will recreate it if needed). */
+
+import { VERCEL_GIT_COMMIT_MESSAGE } from '$env/static/private';
+
+type DeploymentInfo = {
+    commitMeesage: string;
+};
+
+export function load(): DeploymentInfo {
+    return {
+        commitMeesage: VERCEL_GIT_COMMIT_MESSAGE ?? 'local 호스트 입니다.',
+    };
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,15 @@
 	import { onMount } from 'svelte'
 	import BackToTop from "$lib/components/elements/BackToTop.svelte";
 	import Header from "$lib/components/header.svelte";
+	import {Button, Modal} from "flowbite-svelte";
 
 	onMount(() => {
 		if (browser) {
 			import('$lib/utils/tooltip')
 		}
 	})
+	export let data;
+
 </script>
 
 <Header />
@@ -18,4 +21,10 @@
 		<slot />
 	</div>
 	<BackToTop/>
+	<Modal title="Change Log" open autoclose outsideclose>
+		<p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">{data.commitMeesage}</p>
+		<svelte:fragment slot="footer">
+			<Button color="alternative">확인</Button>
+		</svelte:fragment>
+	</Modal>
 </main>
