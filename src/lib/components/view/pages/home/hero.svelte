79a254d01518029514875d4531626b774ae20fc7
hwr12 <ruehyeon12@gmail.com>
2023-11-06 13:21:40 +0900

    a

--- src/lib/components/view/pages/home/hero.svelte
+++ src/lib/components/view/pages/home/hero.svelte
@@ -5,12 +5,12 @@
 	<div class="absolute inset-y-0 left-0 flex items-center w-full justify-center">
 		<div class="flex flex-col md:flex-row w-full px-5 sm:max-w-4xl lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl">
 			<div class="flex-1 flex flex-col gap-8">
-				<h1 class="text-white text-3xl lg:text-4xl 2xl:text-5xl font-bold whitespace-pre-line">
+				<h1 class="text-white text-2xl md:text-3xl lg:text-4xl 2xl:text-5xl font-bold whitespace-pre-line">
 					펫세이프는,
 					반려동물의 안전과 보호를 위한
 					인증제도 입니다
 				</h1>
-				<p class="text-white text-lg font-medium text-slate-200 whitespace-pre-line">
+				<p class="text-white text-sm md:text-lg font-medium text-slate-200 whitespace-pre-line">
 					국제공인분석기관의 엄격한 테스트를 통과한,
 					안전하고 신뢰할 수 있는 제품과 서비스에만 수여하는
 					반려동물 생활용품 안전인증 마크입니다.
