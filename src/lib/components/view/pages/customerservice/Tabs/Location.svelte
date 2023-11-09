b82cee3426eb7b8f9441ae137aaa4b0f28fcb873
hwr12 <ruehyeon12@gmail.com>
2023-11-09 21:29:29 +0900

    update

--- src/lib/components/view/pages/customerservice/Tabs/Location.svelte
+++ src/lib/components/view/pages/customerservice/Tabs/Location.svelte
@@ -1,8 +1,5 @@
 <script>
     import {P} from "flowbite-svelte";
-    import Spacer from "$lib/components/elements/Spacer.svelte";
-    import {onMount} from "svelte";
-    import Map from "$lib/components/view/pages/customerservice/Tabs/Map.svelte";
     export let ready;
 </script>
 
@@ -13,9 +10,6 @@
 <div class="w-[100%] flex flex-col items-start px-4 pb-8 sm:px-[10%]">
     <P class="text-2xl">찾아오시는 길</P>
     <div class="border-gray-30 border-2 rounded-2xl w-full p-6 mt-6 flex flex-col sm:flex-row">
-        <!--{ #if ready }-->
-        <!--    <Map/>-->
-        <!--{ /if }-->
     </div>
 </div>
 
