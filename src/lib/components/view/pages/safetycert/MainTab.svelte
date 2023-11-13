215576a9c939665861b6e5264dd8e8aae39ce80a
hwr12 <ruehyeon12@gmail.com>
2023-11-14 01:24:07 +0900

    change manu sequence

--- src/lib/components/view/pages/safetycert/MainTab.svelte
+++ src/lib/components/view/pages/safetycert/MainTab.svelte
@@ -6,14 +6,6 @@
 </script>
 <div class="w-[100%]">
     <Tabs style={"underline"} defaultClass="w-[100%] flex content-start items-start space-x-2 text-sm font-medium text-center text-primary-text border-b border-gray-200 dark:text-gray-400 dark:border-gray-700 px-[10%]" contentClass="mt-8">
-        <!-- 인증품목 -->
-        <TabItem open title="인증품목" activeClasses="w-[100%] inline-block p-4 border-b-2 border-primary-text rounded-t-lg" >
-            <div class="w-[100%] flex flex-col items-start px-4 sm:px-[10%] pb-4">
-                <img src="/svg/cert_products.svg"/>
-
-            </div>
-        </TabItem>
-
         <!-- 인증소개 -->
         <TabItem title="인증마크소개" activeClasses="w-[100%] inline-block p-4 border-b-2 border-primary-text rounded-t-lg" >
             <div class="w-[100%] flex flex-col items-start px-4 pb-8 sm:px-[10%]">
@@ -53,6 +45,16 @@
                 </div>
             </div>
         </TabItem>
+
+        <!-- 인증품목 -->
+        <TabItem open title="인증품목" activeClasses="w-[100%] inline-block p-4 border-b-2 border-primary-text rounded-t-lg" >
+            <div class="w-[100%] flex flex-col items-start px-4 sm:px-[10%] pb-4">
+                <img src="/svg/cert_products.svg"/>
+
+            </div>
+        </TabItem>
+
+
 
 
     </Tabs>
